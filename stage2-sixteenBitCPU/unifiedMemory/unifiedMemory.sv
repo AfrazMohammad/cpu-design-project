352a0edaa413a956463bfbe9475d375c402f14a9
AfrazMohammad <afrazmohammad00@gmail.com>
2026-07-17 14:35:06 -0400

    Refactor unifiedMemory module with MDR functionality

--- stage2-sixteenBitCPU/unifiedMemory/unifiedMemory.sv
+++ stage2-sixteenBitCPU/unifiedMemory/unifiedMemory.sv
@@ -2,9 +2,10 @@ module unifiedMemory #(
     parameter MEMORY_DEPTH = 65536,
     parameter MEMORY_FILE = "program.mem"
     )(
-    output reg [15:0] read_data,
-    input [15:0] write_data, address,
-    input clk, write_enable
+    output reg [15:0] mdr_data,
+    input [15:0] store_data, address,
+    input mdr_enable, mdr_select, mdr_reset,
+    input clk, memory_write_enable
 );
 
 reg [15:0] memory [0:MEMORY_DEPTH-1];
@@ -15,10 +16,35 @@ initial begin
 end
 
 always @ (posedge clk) begin
-    if (!write_enable)
-        read_data <= memory[address];
-    else
-        memory[address] <= write_data;
+    if (mdr_reset)
+        mdr_data <= 16'h0000;
+    else if (memory_write_enable)
+        memory[address] <= mdr_data;
+    else if (mdr_enable) begin
+        if (!mdr_select)
+            mdr_data <= memory[address];
+        else
+            mdr_data <= store_data;
+    end
 end
 
 endmodule //unifiedMemory
+
+
+
+
+module memoryAddressRegister(
+    output [15:0] address,
+    input [15:0] next_address,
+    input clk, reset, enable
+);
+
+register16bit mar(
+    .q(address),
+    .d(next_address),
+    .clk(clk),
+    .reset(reset),
+    .enable(enable)
+);
+
+endmodule //memoryAddressRegister

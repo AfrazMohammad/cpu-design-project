87a34fae7c8d432ec1b176498067239da43094f8
AfrazMohammad <afrazmohammad00@gmail.com>
2026-09-16 12:51:27 -0400

    Add initial memory values to program.svh
    Initialize memory with predefined values for simulation.

---
+++ runnable-cpu-simulator/program.svh
@@ -0,0 +1,10 @@
+initial begin
+
+	memory[16'h0000] = 16'h3020;
+	memory[16'h0001] = 16'h0025;
+	memory[16'h0002] = 16'hd119;
+	memory[16'h0003] = 16'he9fd;
+	memory[16'h0004] = 16'h8081;
+	memory[16'h0005] = 16'hfc00;
+
+end
